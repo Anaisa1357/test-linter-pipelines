8904426e32f9858f657c61d0601ff8513750a334
Ana Isabel Alzate <isabel.alzate@novacodify.com>
2025-04-23 14:07:23 -0500

    test to push to main

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const mytvarianle = 'hola';
+let hola: string = 'hola';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
